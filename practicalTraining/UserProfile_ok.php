bd91af7e2589c94ff121915c22fbb09c14d5458a
Mr.chen <1294549818@qq.com>
2023-06-05 22:10:43 +0800

    update some file

--- practicalTraining/UserProfile_ok.php
+++ practicalTraining/UserProfile_ok.php
@@ -1,19 +1,19 @@
 <?php
 	require("Conn.php");
 	if(isset($_POST['submit'])){
-		$user = trim($_POST['user']);
-		$cname = trim($_POST['cname']);
+		$user = trim($_POST['username']);
+		$cname = trim($_POST['name']);
 		$city = trim($_POST['city']);
 		$address = trim($_POST['address']);
 		$phone = trim($_POST['phone']);
 		$email = trim($_POST['email']);
 		$zip = trim($_POST['zip']);
-		$sql="update account set Cname='$user',City='$city',Address='$address',Phone='$phone',Email='$email' where Username='$user'";
+		$sql="update account set Cname='$cname',City='$city',Address='$address',Phone='$phone',Email='$email',Zip='$zip' where Username='$user'";
 		$result = $db->query($sql);
 		if($db->affected_rows>=1){
-			echo "<script>alert('更新成功!');location='UserProfile_ok.php'</script>";
+			echo "<script>alert('更新成功!');location='UserProfile.php'</script>";
 		}else{
-			echo "<script>alert('更新失败!');</script>";
+			echo "<script>alert('更新失败!');location='UserProfile.php'</script>";
 		}
 	}
 ?>
